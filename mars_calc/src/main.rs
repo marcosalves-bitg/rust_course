7368761d4409882976ddaf533b33d536a28067cd
Marcos Rodrigo Jung Alves <digosw@gmail.com>
2022-04-04 20:20:47 -0300

    feat: add some mutability prop

--- mars_calc/src/main.rs
+++ mars_calc/src/main.rs
@@ -1,5 +1,9 @@
 fn main() {
-    println!("Weight on Mars: {} Kg", calculate_weight_on_mars(100.0));
+    // If need to mutate a variable, use let mut instead of let
+    // let mut mars_weight = calculate_weight_on_mars(100.0);
+    // mars_weight = mars_weight * 1000.0;
+    let mars_weight = calculate_weight_on_mars(100.0);
+    println!("Weight on Mars: {} Kg", mars_weight);
 }
 
 fn calculate_weight_on_mars (weight: f32) -> f32 {
